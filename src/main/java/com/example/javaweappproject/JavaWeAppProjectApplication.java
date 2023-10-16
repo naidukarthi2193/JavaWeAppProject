4ab8163cab0ab9e8438255daa0217a2d16bfe3f3
naidukarthi2193 <karthikraj.venkatraj@truweathersolutions.com>
2023-10-16 17:12:16 -0400

    init commit

---
+++ src/main/java/com/example/javaweappproject/JavaWeAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaweappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWeAppProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWeAppProjectApplication.class, args);
+    }
+
+}
